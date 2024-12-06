bcef38480057ab9b153240ca58cf064cc354e191
Andrew Tiu <63322884+winter-again@users.noreply.github.com>
2024-12-06 16:40:24 -0500

    Bad

---
+++ aoc-2024/day06/main_test.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/winter-again/advent-of-code/aoc-2024/util"
+)
+
+func TestPart1(t *testing.T) {
+	tests := []util.TestCase{
+		{
+			Name:   "Part 1 (sample)",
+			Input:  "./input_smpl.txt",
+			Expect: 41,
+		},
+		{
+			Name:   "Part 1",
+			Input:  "./input.txt",
+			Expect: 5444,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.Name, func(t *testing.T) {
+			if got := solvePart1(test.Input); got != test.Expect {
+				t.Errorf("%s: expected %d but got %d", test.Name, test.Expect, got)
+			}
+		})
+	}
+}
+
+func TestPart2(t *testing.T) {
+	tests := []util.TestCase{
+		{
+			Name:   "Part 2 (sample)",
+			Input:  "./input_smpl.txt",
+			Expect: 6,
+		},
+		{
+			Name:   "Part 2",
+			Input:  "./input.txt",
+			Expect: 9999,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.Name, func(t *testing.T) {
+			if got := solvePart2(test.Input); got != test.Expect {
+				t.Errorf("%s: expected %d but got %d", test.Name, test.Expect, got)
+			}
+		})
+	}
+}
