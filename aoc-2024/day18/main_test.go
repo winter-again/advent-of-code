3984a206b6b3dfbfcf09603c165cf3f58a8d2d5e
Andrew Tiu <63322884+winter-again@users.noreply.github.com>
2024-12-18 15:39:00 -0500

    Not bad. Good practice.

--- aoc-2024/day18/main_test.go
+++ aoc-2024/day18/main_test.go
@@ -11,22 +11,25 @@ func TestPart1(t *testing.T) {
 		{
 			Name:   "Part 1 (sample)",
 			Input:  "./input_smpl.txt",
-			Expect: 9999,
+			Expect: 22,
 		},
 		{
 			Name:   "Part 1",
 			Input:  "./input.txt",
-			Expect: 9999,
+			Expect: 348,
 		},
 	}
 
-	for _, test := range tests {
-		t.Run(test.Name, func(t *testing.T) {
-			if got := solvePart1(test.Input); got != test.Expect {
-				t.Errorf("%s: expected %d but got %d", test.Name, test.Expect, got)
-			}
-		})
-	}
+	t.Run(tests[0].Name, func(t *testing.T) {
+		if got := solvePart1(tests[0].Input, true); got != tests[0].Expect {
+			t.Errorf("%s: expected %d but got %d", tests[0].Name, tests[0].Expect, got)
+		}
+	})
+	t.Run(tests[1].Name, func(t *testing.T) {
+		if got := solvePart1(tests[1].Input, false); got != tests[1].Expect {
+			t.Errorf("%s: expected %d but got %d", tests[1].Name, tests[1].Expect, got)
+		}
+	})
 }
 
 func TestPart2(t *testing.T) {
