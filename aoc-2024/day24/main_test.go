ee9a9402f06ddc1b5ee9adeb968dd22016d52326
Andrew Tiu <63322884+winter-again@users.noreply.github.com>
2024-12-28 19:31:09 -0500

    Day 24 part 2

---
+++ aoc-2024/day24/main_test.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/winter-again/advent-of-code/aoc-2024/util"
+)
+
+func TestPart1(t *testing.T) {
+	tests := []util.TestCase{
+		{
+			Name:   "Part 1 (sample)",
+			Input:  "./input_smpl.txt",
+			Expect: 4,
+		},
+		{
+			Name:   "Part 1 (sample 2)",
+			Input:  "./input_smpl_2.txt",
+			Expect: 2024,
+		},
+		{
+			Name:   "Part 1",
+			Input:  "./input.txt",
+			Expect: 45121475050728,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.Name, func(t *testing.T) {
+			if got := solvePart1(test.Input); got != test.Expect {
+				t.Errorf("%s: expected %d but got %d", test.Name, test.Expect, got)
+			}
+		})
+	}
+}
+
+func TestPart2(t *testing.T) {
+	tests := []util.TestCaseDay17Part1{
+		{
+			Name:   "Part 2 (sample)",
+			Input:  "./input_smpl_3.txt",
+			Expect: "z00,z01,z02,z05",
+		},
+		{
+			Name:   "Part 2",
+			Input:  "./input.txt",
+			Expect: "",
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.Name, func(t *testing.T) {
+			if got := solvePart2(test.Input); got != test.Expect {
+				t.Errorf("%s: expected %s but got %s", test.Name, test.Expect, got)
+			}
+		})
+	}
+}
