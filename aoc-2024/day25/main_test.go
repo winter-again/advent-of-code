43e0c43dd0825a74803e6cb2caae65354b4c4a0f
Andrew Tiu <63322884+winter-again@users.noreply.github.com>
2024-12-31 20:45:01 -0500

    Done

--- aoc-2024/day25/main_test.go
+++ aoc-2024/day25/main_test.go
@@ -28,26 +28,3 @@ func TestPart1(t *testing.T) {
 		})
 	}
 }
-
-func TestPart2(t *testing.T) {
-	tests := []util.TestCase{
-		{
-			Name:   "Part 2 (sample)",
-			Input:  "./input_smpl.txt",
-			Expect: 9999,
-		},
-		{
-			Name:   "Part 2",
-			Input:  "./input.txt",
-			Expect: 9999,
-		},
-	}
-
-	for _, test := range tests {
-		t.Run(test.Name, func(t *testing.T) {
-			if got := solvePart2(test.Input); got != test.Expect {
-				t.Errorf("%s: expected %d but got %d", test.Name, test.Expect, got)
-			}
-		})
-	}
-}
