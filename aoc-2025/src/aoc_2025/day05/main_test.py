5c6b18df4ef6408db17d1e3a4319df81372bf7f5
Andrew Tiu <63322884+winter-again@users.noreply.github.com>
2025-12-05 08:20:07 -0500

    Day 05

---
+++ aoc-2025/src/aoc_2025/day05/main_test.py
@@ -0,0 +1,21 @@
+from .main import part_1, part_2
+
+
+def test_part_1_sample() -> None:
+    ans = part_1(True)
+    assert ans == 3
+
+
+def test_part_1() -> None:
+    ans = part_1()
+    assert ans == 513
+
+
+def test_part_2_sample() -> None:
+    ans = part_2(True)
+    assert ans == 14
+
+
+def test_part_2() -> None:
+    ans = part_2()
+    assert ans == 339668510830757
