a5608e176f628f1aab04c85c8c99d4c3e689b010
Andrew Tiu <63322884+winter-again@users.noreply.github.com>
2025-12-06 13:15:10 -0500

    Use better params from 2025

--- aoc-2023/src/aoc_2023/day11/main.py
+++ aoc-2023/src/aoc_2023/day11/main.py
@@ -8,18 +8,25 @@ parent = Path(__file__).parent
 def main(argv: Sequence[str] | None = None) -> int:
     parser = argparse.ArgumentParser()
     parser.add_argument("part", nargs="?", default=0, type=int)
+    parser.add_argument(
+        "-s", "--sample-only", action=argparse.BooleanOptionalAction, default=False
+    )
     args = parser.parse_args(argv)
 
     if args.part == 0:
         print(f"Part 1(sample): {part_1(True)}")
         print(f"Part 1: {part_1()}")
 
+        print(f"Part 2 (sample): {part_2(True)}")
         print(f"Part 2: {part_2()}")
     elif args.part == 1:
         print(f"Part 1 (sample): {part_1(True)}")
-        print(f"Part 1: {part_1()}")
+        if not args.sample_only:
+            print(f"Part 1: {part_1()}")
     elif args.part == 2:
-        print(f"Part 2: {part_2()}")
+        print(f"Part 2 (sample): {part_2(True)}")
+        if not args.sample_only:
+            print(f"Part 2: {part_2()}")
 
     return 0
 
